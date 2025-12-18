9552b21dcac1ef0079f64a439348d8bfd8a62edf
Alcides Turruellas <ayunierto@gmail.com>
2025-12-18 17:03:48 -0500

    feat: make address and durationMinutes required fields in Service interface

--- src/interfaces/services.interfaces.ts
+++ src/interfaces/services.interfaces.ts
@@ -4,21 +4,22 @@ import {
   SimpleStaffMember,
   SimpleAppointment,
 } from './simple-types.interfaces';
+import { StaffMember } from './staff-members.interfaces';
 
 // Service Entity
 export interface Service {
   id: string;
   name: string;
   description: string;
-  address?: string;
-  durationMinutes?: number;
+  address: string;
+  durationMinutes: number;
   isAvailableOnline: boolean;
   imageUrl?: string;
   isActive: boolean;
   createdAt: string;
   updatedAt: string;
   deletedAt?: string;
-  staffMembers?: SimpleStaffMember[];
+  staffMembers?: StaffMember[];
   appointments?: Appointment[];
 }
 
