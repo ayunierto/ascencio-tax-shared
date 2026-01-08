e54237a5f1b342643a71a8f2c37d9f89948cab06
Alcides Turruellas <ayunierto@gmail.com>
2026-01-08 13:50:22 -0500

    Updated shared package

--- src/interfaces/upload-image-file.interfaces.ts
+++ src/interfaces/upload-image-file.interfaces.ts
@@ -2,3 +2,14 @@ export interface UploadImageFile {
   uploadUrl: string;
   mediaToken: string;
 }
+
+// Payload to allow clients to upload directly to Cloudinary using a signed request.
+export interface UploadSignaturePayload {
+  uploadUrl: string;
+  cloudName: string;
+  apiKey: string;
+  timestamp: number;
+  signature: string;
+  folder: string;
+  publicId: string;
+}
