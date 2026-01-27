179cc5cff2aa748e91305c9ae6aac9a1a35b9c1a
Alcides Turruellas <ayunierto@gmail.com>
2026-01-26 21:32:41 -0500

    Updated shared package

--- src/schemas/users.schemas.ts
+++ src/schemas/users.schemas.ts
@@ -1,13 +1,13 @@
-import { z } from 'zod';
-import { ValidationMessages as ValMsgs } from '../i18n';
+import { z } from "zod";
+import { ValidationMessages as ValMsgs } from "../i18n";
 import {
   countryCodeSchema,
   emailSchema,
   firstNameSchema,
   lastNameSchema,
   phoneNumberSchema,
-} from './common.schemas';
-import { passwordSchema } from './auth';
+} from "./common.schemas";
+import { passwordSchema } from "./auth";
 
 // Users
 export const createUserSchema = z.object({
@@ -33,11 +33,7 @@ export const updateProfileSchema = z.object({
   lastName: lastNameSchema,
   countryCode: countryCodeSchema.optional(),
   phoneNumber: phoneNumberSchema.optional(),
-  password: z
-    .string()
-    .min(8, ValMsgs.MIN_LENGTH)
-    .max(100, ValMsgs.MAX_LENGTH)
-    .optional(),
+  password: passwordSchema.optional(),
 });
 
 export type UpdateProfileRequest = z.infer<typeof updateProfileSchema>;
